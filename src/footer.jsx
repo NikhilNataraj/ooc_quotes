d06eadaf4463847597d7bdcea9c86741d667814e
Nikhil <nikhil.nataraj@gmail.com>
2026-02-03 16:43:29 +0530

    Footer

---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <footer className="footer-bar">
+        <span>Ⓒ {new Date().getFullYear()} Out-of-Context ADSA Archives | Developed by Anonymous Archive-keepers </span>
+      </footer>
+    </div>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import Footer from './footer.jsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>
 )
