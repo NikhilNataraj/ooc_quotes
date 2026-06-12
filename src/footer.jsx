0590ce97c2b75ebd61af09a036c793809606d012
Nikhil <nikhil.nataraj@gmail.com>
2026-06-12 12:10:18 +0530

    Display changes to disclaimer page

--- src/footer.jsx
+++ src/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="footer-container">
       <footer className="footer-bar">
-        <span>Ⓒ {new Date().getFullYear()} Out-of-Context ADSA Archives | Developed by Anonymous Archive-keepers </span>
+        <span>Ⓒ {new Date().getFullYear()} Out-of-Context Quotes Archives | Developed by Anonymous Archive-keepers </span>
       </footer>
     </div>
     );
